cb43740a95f986fcb6cbbedf09a5d2c815468aec
RohitKhavanekar <rohit.oyc9@gmail.com>
2020-06-23 13:06:46 +0530

    New Changed

--- src/GlobalValuesDeclar/GlobalValues.java
+++ src/GlobalValuesDeclar/GlobalValues.java
@@ -26,6 +26,7 @@ public class GlobalValues {
 		System.out.println("Git Test Checking");
 		System.out.println("Git Test Checking");
 		System.out.println("Git Test Checking");
+		System.out.println("Changed for checking");
 		
 		
 
