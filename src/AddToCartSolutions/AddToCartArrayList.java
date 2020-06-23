a032f841bf6c1d03bfd463944dbdc0e1c2d5349c
RohitKhavanekar <rohit.oyc9@gmail.com>
2020-06-23 10:34:52 +0530

    Commit message

--- src/AddToCartSolutions/AddToCartArrayList.java
+++ src/AddToCartSolutions/AddToCartArrayList.java
@@ -43,6 +43,10 @@ public class AddToCartArrayList {
 
 		}
 		System.out.println("print 1");
+		System.out.println("print 2");
+		System.out.println("print 3");
+		System.out.println("print 4");
+		System.out.println("print 5");
 	}
 	
 
